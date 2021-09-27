bcd3f34b879b29b6ae6b16a84d8af21710246961
Mandi Walls <mandi.walls@gmail.com>
2021-09-27 14:04:20 -0400

    changing the format of the resources that are included in the custom info of the change event

--- garage-repo/cookbooks/example/recipes/default.rb
+++ garage-repo/cookbooks/example/recipes/default.rb
@@ -1,8 +1,18 @@
 # This is a Chef Infra Client recipe file. It can be used to specify resources
 # which will apply configuration to a server.
+require 'time'
+
+time = Time.now()
 
 log "Welcome to Chef Infra Client, #{node['example']['name']}!" do
   level :info
 end
 
+file "/tmp/timer1" do
+  content '#{time}'
+end
+
+file "/tmp/timer3" do
+  content '#{time}'
+end
 # For more information, see the documentation: https://docs.chef.io/recipes.html
--- garage-repo/cookbooks/pagerduty-handler/recipes/default.rb
+++ garage-repo/cookbooks/pagerduty-handler/recipes/default.rb
@@ -4,6 +4,12 @@
 #
 # Copyright:: 2021, The Authors, All Rights Reserved.
 #
+require 'time'
+
+time = Time.now()
+file "/tmp/timer2" do
+  content '#{time}'
+end
 
 cookbook_file '/tmp/change_events.rb' do
   source 'change_events.rb'
@@ -12,6 +18,8 @@ end
 cookbook_file '/tmp/dummy_handler.rb' do
   source 'dummy_handler.rb'
 end
+
+
 
 # chef_handler 'SimpleReport::UpdatedResources' do
 #   source '/tmp/dummy_handler.rb'
