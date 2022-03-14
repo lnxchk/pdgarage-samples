c540d9aa44d4091ccf8d3de58c4d049e87b9b669
Mandi Walls <mandi.walls@gmail.com>
2022-03-14 10:22:05 -0400

    Change Event script runs directly. Fixed ToDo.

--- change_event.sh
+++ change_event.sh
@@ -11,8 +11,6 @@
 # will print out the request body
 # copy and past on the command line
 #
-# ToDo:
-# figure out why curl won't run this input as-is
 # ==========================================
 
 # timestamp for the event
@@ -22,33 +20,39 @@ BUILD_NUMBER=`date +%s`
 
 # Your token. This is an Account-level credential
 # https://youraccount.pagerduty.com/api_keys
-TOKEN=".+............nBfzBQ"
+TOKEN=$PD_API_KEY
 
 # valid email for a user in your PagerDuty account
-EMAIL="user@mail.com"
+EMAIL=$PD_FROM_ADDR
 
 # integration key
 # this is a SERVICE LEVEL integration
 # you need a different key for each destination service
 # https://youraccount.pagerduty.com/change-events
-KEY="289..........................916"
+KEY=""
 
-cat <<EOF
+data=$(cat <<EOF
+  {
+    "routing_key": "$KEY",
+    "payload": {
+      "summary": "Build Success: Build has Passed.",
+      "timestamp": "$DATE",
+      "source": "amazing-build-pipeline-thing",
+      "custom_details": {
+        "build_state": "passed",
+        "build_number": "$BUILD_NUMBER",
+        "build_developer": "Jill Developer"
+      }
+    }
+  }
+EOF
+)
+
+
 curl -X POST --header 'Content-Type: application/json' \
 --url https://events.pagerduty.com/v2/change/enqueue \
 --header 'Accept: application/vnd.pagerduty+json;version=2' \
 --header 'Authorization: Token token=$TOKEN' \
---data '{
-  "routing_key": "$KEY",
-  "payload": {
-    "summary": "Build Success: Build has Passed.",
-    "timestamp": "$DATE",
-    "source": "amazing-build-pipeline-thing",
-    "custom_details": {
-      "build_state": "passed",
-      "build_number": "$BUILD_NUMBER",
-      "build_developer": "Jill Developer"
-    }
-  }
-}'
-EOF
+--data "$data"
+
+echo
