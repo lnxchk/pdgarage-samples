a78b810fccfab79e6713afbba2ad8ebdfa6d03f2
Mandi Walls <mandi.walls@gmail.com>
2022-08-29 10:34:04 -0400

    Adding functionality to the send_event script to use an argument.
    The arg will be the name of a local json file for the event payload.
    Included here are three new events and a template.

--- send_event.sh
+++ send_event.sh
@@ -1,15 +1,13 @@
 # EXAMPLE:
-# create a generic change event on a service.
+# create a generic event on a service.
 
 # ==========================================
 # Documentation:
-# https://support.pagerduty.com/docs/change-events
-# https://developer.pagerduty.com/api-reference/reference/events-v2/openapiv3.json/paths/~1change~1enqueue/post
 #
 # Usage:
-# ./change_event.sh 
+# ./send_event.sh
 # will print out the request body
-# copy and past on the command line
+# and the response from the API.
 #
 # ==========================================
 
@@ -29,30 +27,18 @@ EMAIL=$PD_FROM_ADDR
 # this is a SERVICE LEVEL integration
 # you need a different key for each destination service
 # https://youraccount.pagerduty.com/change-events
-KEY=$PD_ROUTE_KEY
+export KEY=$PD_ROUTE_KEY
 
-data=$(cat <<EOF
-  {
-    "routing_key": "$KEY",
-    "payload": {
-      "summary": "Build Success: Build has Passed.",
-      "timestamp": "$DATE",
-      "source": "amazing-build-pipeline-thing",
-      "custom_details": {
-        "build_state": "passed",
-        "build_number": "$BUILD_NUMBER",
-        "build_developer": "Jill Developer"
-      }
-    }
-  }
-EOF
-)
+# event template file
+# a local json file that contains the payload of the event type
+TEMPLATE=$1
+
+data=`envsubst < $TEMPLATE`
+
 echo $data
 
 curl -X POST --header 'Content-Type: application/json' \
---url https://events.pagerduty.com/v2/change/enqueue \
---header 'Accept: application/vnd.pagerduty+json;version=2' \
---header 'Authorization: Token token=$TOKEN' \
+--url https://events.pagerduty.com/v2/enqueue \
 --data "$data"
 
 echo
